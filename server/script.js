4c2ffffe2405a471e30cb2167c3879d691c71018
HerschelW <herschelw88@gmail.com>
2020-05-27 13:46:09 -0500

    tree structure changes

--- server/script.js
+++ server/script.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-   res.redirect("/");
-});
-
 app.listen(3000, function () {
    console.log("Server started on port 3000");
    console.log(Date());
